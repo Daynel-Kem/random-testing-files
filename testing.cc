07a9e7149c1da14f0edc502625a2a77486ffe2c1
Daynel-Kem <dani3lkimm2005@gmail.com>
2025-01-15 18:40:38 -0500

    Create testing.cc

---
+++ testing.cc
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string test = argv[1];
+    cout << test;
+    return 0;
+}
