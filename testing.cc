6f4623d433f25541eff28d47b23f6e7a016af7b1
Daynel-Kem <dani3lkimm2005@gmail.com>
2025-02-05 17:23:16 -0500

    stuff

--- testing.cc
+++ testing.cc
@@ -2,10 +2,28 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <math.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string test = argv[1];
-    cout << test;
+    vector<float> sequence;
+    vector<float> angles;
+    angles.emplace_back(0);
+    sequence.emplace_back(360);
+    float a = 360;
+    float b;
+    for (int i = 1; i <= 10; ++i) {
+        b = round((a - 10 * pow(1.22, i)) * 10000) / 10000;
+        sequence.emplace_back(b);
+        angles.emplace_back(10 * pow(1.22, i));
+        a = b;
+    }
+    
+    int l = 0;
+    for (auto it = sequence.begin(); it != sequence.end(); ++it) {
+        cout << *it << " slices: " << l << " Angle: " << angles[l] << endl;
+        ++l;
+    }
     return 0;
 }
